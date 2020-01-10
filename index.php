7e12e42be131e6d9e874770c4ad938e88bc3dc6c
Votre Nom <odilecoutiez@gmail.com>
2020-01-10 16:19:12 +0100

    Exercices PHP partie-09 sur Les dates

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+        <title>Partie-IX</title>
+    </head>
+    <body>
+        <div class="page text-center">
+            <h1>PHP PARTIE IX <span>"Les Dates"</span></h1>
+            <div class="row">
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher la date courante en respectant la forme jj/mm/aaaa (ex : 16/05/2016)</p>
+                    <button type="button"><a href="exo-1/index.php">Exercice-1</a></button>
+                </div>
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher la date courante en respectant la forme jj-mm-aa (ex : 16-05-16)</p>
+                    <button type="button"><a href="exo-2/index.php">Exercice-2</a></button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher la date courante avec le jour de la semaine et le mois en toutes lettres (ex : mardi 2 août 2016)
+                    Bonus : Le faire en français.</p>
+                    <button type="button"><a href="exo-3/index.php">Exercice-3</a></button>
+                </div>
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher le timestamp du jour.
+                     Afficher le timestamp du mardi 2 août 2016 à 15h00.</p>
+                    <button type="button"><a href="exo-4/index.php">Exercice-4</a></button>
+                </div> 
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher le nombre de jour qui sépare la date du jour avec le 16 mai 2016.</p>
+                    <button type="button"><a href="exo-5/index.php">Exercice-5</a></button>
+                </div>
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher le nombre de jour dans le mois de février de l'année 2016.</p>
+                    <button type="button"><a href="exo-6/index.php">Exercice-6</a></button>
+                </div> 
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher la date du jour + 20 jours.</p> 
+                    <button type="button"><a href="exo-7/index.php">Exercice-7</a></button>
+                </div>
+                <div class="col-md-6 text-center mt-3 mb-3">
+                    <p>Afficher la date du jour - 22 jours</p>
+                    <button type="button"><a href="exo-8/index.php">Exercice-8</a></button>
+                </div> 
+            </div>
+             <div class="row">
+                <div class="col-md-12 text-center mt-3 mb-3">
+                    <p>Faire un formulaire avec deux listes déroulantes. La première sert à choisir le mois, et le deuxième permet d'avoir l'année.
+                   En fonction des choix, afficher un calendrier</p> 
+                    <button type="button"><a href="tp/index.php">Tp</a></button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
